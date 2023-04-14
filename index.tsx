c295e58c529f78d25bc5e1d0ab34d375f6ad9efd
dicky <dicky.a@weefer.co.id>
2023-04-14 08:35:13 +0700

    [DA] - Clean code

--- index.tsx
+++ index.tsx
@@ -1,10 +1,10 @@
-import FreshchatConfig from "react-native-freshchat-sdk/components/freshchatConfig";
-import FaqOptions from "react-native-freshchat-sdk/components/faqOptions";
-import ConversationOptions from "react-native-freshchat-sdk/components/conversationOptions";
-import FreshchatUser from "react-native-freshchat-sdk/components/freshchatUser";
-import FreshchatMessage from "react-native-freshchat-sdk/components/freshchatMessage";
-import FreshchatNotificationConfig from "react-native-freshchat-sdk/components/freshchatNotificationConfig";
-import FreshchatJSWrapper from "react-native-freshchat-sdk/components/freshchatJSWrapper";
+import FreshchatConfig from "./components/freshchatConfig";
+import FaqOptions from "./components/faqOptions";
+import ConversationOptions from "./components/conversationOptions";
+import FreshchatUser from "./components/freshchatUser";
+import FreshchatMessage from "./components/freshchatMessage";
+import FreshchatNotificationConfig from "./components/freshchatNotificationConfig";
+import FreshchatJSWrapper from "./components/freshchatJSWrapper";
 
 const FreshchatComponents = {
   Freshchat: FreshchatJSWrapper,
